64e988195d1fd1f1bf9a4b62528bf651e5a84f00
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-11-26 16:20:04 -0500

    added weatherCard component

--- src/components/weatherCards/weatherContainer.js
+++ src/components/weatherCards/weatherContainer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { OPEN_WEATHER_KEY } from '../../apiKey';
+import WeatherDay from './weatherDay';
+
+const openWeatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=60525&units=imperial&APPID=${OPEN_WEATHER_KEY}`;
 
 export default function weatherContainer() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <WeatherDay />
     </div>
   );
 }
--- src/components/weatherCards/weatherDay.js
+++ src/components/weatherCards/weatherDay.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import styles from './weatherCardStyle.module.css';
 
-export default function weatherDay() {
-  return <div>
-    
-  </div>;
+function weatherDay() {
+  return (
+    <div className={styles.card}>
+      <h2>Brussels</h2>
+      <h3>
+        Cloudy
+        <span>
+          Wind 10km/h <span className={styles.dot}>•</span> Precipitation 0%
+        </span>
+      </h3>
+      <h1>23°</h1>
+      <div className={styles.sky}>
+        <div className={styles.sun}></div>
+        <div className={styles.cloud}>
+          <div className="circle-small"></div>
+          <div className="circle-tall"></div>
+          <div className="circle-medium"></div>
+        </div>
+      </div>
+      <table>
+        <tr>
+          <td>TUE</td>
+          <td>WED</td>
+          <td>THU</td>
+          <td>FRI</td>
+          <td>SAT</td>
+        </tr>
+        <tr>
+          <td>30°</td>
+          <td>34°</td>
+          <td>36°</td>
+          <td>34°</td>
+          <td>37°</td>
+        </tr>
+        <tr>
+          <td>17°</td>
+          <td>22°</td>
+          <td>19°</td>
+          <td>23°</td>
+          <td>19°</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
+
+export default weatherDay;
